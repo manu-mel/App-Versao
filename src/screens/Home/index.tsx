9bbae0ffc4cd9fa40f4516b3b60be49f6d01a07e
Emanuel <emanuel@exciasistemas.com.br>
2024-04-26 13:32:43 -0300

    api

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Label } from './styles';
+import api from '../../services/api';
+import Button from '../../components/Button';
+import { Alert, Text } from 'react-native';
+
+interface Product {
+  id: string;
+  title: string;
+}
 
 const Home = () => {
+  const [products, setProducts] = useState<Array<Product>>([]);
+
+  const handleConsulta = async () => {
+    try {
+      const response = await api.get('/products/1');
+
+      // const data = response.data.map(item => {
+      //   return {
+      //     id: item.id,
+      //     title: item.title,
+      //   };
+      // });
+
+      const data = response.data;
+      setProducts(data);
+    } catch (error: any) {
+      Alert.alert(error.message);
+    }
+  };
+
   return (
     <>
-      <Label>Bem vindo ao App</Label>
+      <Label>Home</Label>
+      <Button label={'Consultar'} onPress={handleConsulta} />
+      {/* {products?.map(item => {
+        <Text>{item.title}</Text>;
+      })} */}
+      <Text>{products.title}</Text>
     </>
   );
 };
