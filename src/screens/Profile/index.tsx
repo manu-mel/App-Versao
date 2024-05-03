1223957c7f256990b083c4644012d715d172b2c5
Emanuel <emanuel@exciasistemas.com.br>
2024-05-03 15:50:18 -0300

    profile

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,10 +1,92 @@
-import React from 'react';
-import { Label } from './styles';
+import React, { useEffect, useState } from 'react';
+import { Container, Label, Title } from './styles';
+import { Alert } from 'react-native';
+import api from '../../services/api';
+import { Bold } from './styles';
+
+interface IName {
+  firstname: string;
+  lastname: string;
+}
+
+interface IAddress {
+  city: string;
+  street: string;
+  number: number;
+  zipcode: string;
+}
+
+interface IUser {
+  id: number;
+  email: string;
+  username: string;
+  password: string;
+  name: IName;
+  address: IAddress;
+  phone: string;
+}
 
 const Profile = () => {
+  const [user, setUser] = useState<IUser>();
+  const loadUser = async () => {
+    try {
+      const response = await api.get('/users/1');
+      const data = response.data;
+
+      setUser(data);
+    } catch (error: any) {
+      Alert.alert(error.message);
+    }
+  };
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
   return (
     <>
-      <Label>Profile</Label>
+      <Title>Personal Information</Title>
+      <Container>
+        <Label>
+          <Bold>Name: </Bold>
+          {user?.name.firstname}
+        </Label>
+        <Label>
+          <Bold>Last Name: </Bold>
+          {user?.name.lastname}
+        </Label>
+        <Label>
+          <Bold>User Name: </Bold>
+          {user?.username}
+        </Label>
+        <Label>
+          <Bold>E-mail: </Bold>
+          {user?.email}
+        </Label>
+        <Label>
+          <Bold>Phone: </Bold>
+          {user?.phone}
+        </Label>
+      </Container>
+      <Title>Address Information</Title>
+      <Container>
+        <Label>
+          <Bold>Number: </Bold>
+          {user?.address.number}
+        </Label>
+        <Label>
+          <Bold>Street: </Bold>
+          {user?.address.street}
+        </Label>
+        <Label>
+          <Bold>Zip Code: </Bold>
+          {user?.address.zipcode}
+        </Label>
+        <Label>
+          <Bold>City: </Bold>
+          {user?.address.city}
+        </Label>
+      </Container>
     </>
   );
 };
--- src/screens/Profile/styles.tsx
+++ src/screens/Profile/styles.tsx
@@ -1,8 +1,23 @@
 import styled from 'styled-components/native';
 
 export const Label = styled.Text`
-  text-align: center;
   font-size: 25px;
   color: ${({ theme }) => theme.colors.labelBox};
-  margin-bottom: 10px;
+  margin: 15px 30px;
+`;
+
+export const Bold = styled.Text`
+  font-weight: bold;
+`;
+
+export const Container = styled.View`
+  border: 1px;
+  margin: 0px 15px 0px 15px;
+`;
+
+export const Title = styled.Text`
+  font-size: 25px;
+  font-weight: bold;
+  color: ${({ theme }) => theme.colors.labelBox};
+  margin: 20px;
 `;
